3421a72031c72d90057af011c8cd9c07cfd33e20
Miguel Olarte <mbsic91@gmail.com>
2025-01-28 13:30:57 -0500

    feat: Se modifica el método muestraMenu()

--- src/Simulador.java
+++ src/Simulador.java
@@ -1,51 +1,64 @@
 import java.util.Scanner;
-public class Simulador {
-    static String[] plantetas ={"Mercurio", "Venus", "Marte", "Júpiter", "Saturno"};
-    static double[] distancias ={};
 
+public class Simulador {
+    String[] plantetas = { "Mercurio", "Venus", "Marte", "Júpiter", "Saturno", "Urano", "Neptuno" };
+    double[] distancias = {};
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-       int  opcion;
-        do {
-            muestraMenu();
-            opcion = scanner.nextInt();
-            switch (opcion) {
-                case 1: //Planeta
-                    break;
-                case 2: //Nave
-                    break;
-                case 3: //Simulación
-                    break;
-                case 4: //Salir
-                System.out.println("Gracias por visitar");
-                    break;
-                default:
-                System.out.println("Opción no válida\n");
-                    break;
-            }
-        } while (opcion!=4);
-        scanner.close();
+        muestraMenu();
     }
 
+    static void muestraMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
 
-    private static void muestraMenu(){
         System.out.println("Bienvenido a este viaje interplanetario. Elige tu próxima travesía:");
         System.out.println("1. Seleccionar un planeta de destino.");
-        System.out.println("2. Seleccionar una nave espacial");
-        System.out.println("3. Iniciar la simulación del viaje");
-        System.out.println("4. Salir del programa");
+        System.out.println("2. Calcular distancia y tiempo de viaje");
+        System.out.println("3. Gestionar recursos de la nave");
+        System.out.println("4. Monitorear estado del viaje");
+        System.out.println("5. Salir del programa");
+
+        opcion = scanner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                seleccionarPlaneta();
+                break;
+            case 2:
+                calcularDistanciaTiempo();
+                break;
+            case 3:
+                gestionarRecursos();
+                break;
+            case 4:
+                monitorearEstado();
+                break;
+            case 5: // Salir
+                System.out.println("Gracias por visitar");
+                break;
+            default:
+                System.out.println("Opción no válida\n");
+                muestraMenu();
+                break;
+        }
+        scanner.close();
+
     }
 
-    private static void seleccionarPlaneta(){ //Método para planetas
-        
+    static void seleccionarPlaneta() {
+        System.out.println("Seleccionar planeta");
     }
 
-    private static void gestionarNave(){ //Método para la nave
-
+    static void calcularDistanciaTiempo() {
+        System.out.println("Calcular distancia y tiempo de viaje");
     }
 
-    private static void iniciarSimulacion(){ //Método de simulación
+    static void gestionarRecursos() {
+        System.out.println("Gestionar Recursos de la nave");
+    }
 
+    static void monitorearEstado() {
+        System.out.println("Monitorear estado del viaje");
     }
 }
