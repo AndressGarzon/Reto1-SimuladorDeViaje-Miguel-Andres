b9b50055d031b3510af55286185d641113ff73a5
AndressGarzon <andresgarzon0422@gmail.com>
2025-01-28 18:46:08 -0500

    feat: Cambio en el método main. El menú se pasa a su propio método y se agregan los planetas faltantes

--- src/Simulador.java
+++ src/Simulador.java
@@ -1,41 +1,50 @@
 import java.util.Scanner;
-public class Simulador {
-    static String[] plantetas ={"Mercurio", "Venus", "Marte", "Júpiter", "Saturno"};
-    static double[] distancias ={};
 
+public class Simulador {
+    static String[] planetas = { "Mercurio", "Venus", "Marte", "Júpiter", "Saturno", "Urano", "Neptuno" };
+    static double[] distancias = {};
+    static String[] naves = {};
 
     public static void main(String[] args) {
+        muestraMenu();
+    }
+
+    private static void muestraMenu() {
         Scanner scanner = new Scanner(System.in);
-       int  opcion;
+        int opcion;
+
         do {
-            muestraMenu();
+            System.out.println("Bienvenido a este viaje interplanetario. Elige tu próxima travesía:");
+            System.out.println("1. Seleccionar un planeta de destino.");
+            System.out.println("2. Seleccionar una nave espacial.");
+            System.out.println("3. Iniciar la simulación del viaje.");
+            System.out.println("4. Salir del programa.");
+            System.out.print("Ingresa una opción: ");
+
             opcion = scanner.nextInt();
+
             switch (opcion) {
-                case 1: //Planeta
+                case 1:
+                    seleccionarPlaneta();
                     break;
-                case 2: //Nave
+                case 2:
+                    gestionarNave();
                     break;
-                case 3: //Simulación
+                case 3:
+                    iniciarSimulacion();
                     break;
-                case 4: //Salir
-                System.out.println("Gracias por visitar");
+                case 4:
+                    System.out.println("Gracias por visitar.");
                     break;
                 default:
-                System.out.println("Opción no válida\n");
+                    System.out.println("Opción no válida. Por favor, intenta de nuevo.\n");
                     break;
             }
-        } while (opcion!=4);
+        } while (opcion != 4);
+
         scanner.close();
     }
 
-
-    private static void muestraMenu(){
-        System.out.println("Bienvenido a este viaje interplanetario. Elige tu próxima travesía:");
-        System.out.println("1. Seleccionar un planeta de destino.");
-        System.out.println("2. Seleccionar una nave espacial");
-        System.out.println("3. Iniciar la simulación del viaje");
-        System.out.println("4. Salir del programa");
-    }
 
     private static void seleccionarPlaneta(){ //Método para planetas
         
